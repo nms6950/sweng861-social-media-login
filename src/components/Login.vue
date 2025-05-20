51fdb6148a422760ae7faed8136152d1c0eedcd5
nms6950 <nms6950@psu.edu>
2025-05-19 20:23:24 -0400

    started UI work

---
+++ src/components/Login.vue
@@ -0,0 +1,92 @@
+<template>
+    <div class="container">
+        <div class="welcome-msg">
+            >> {{ visibleWelcomeMsg }}
+            <div class="terminal-box" :class="flicker ? 'visible' : 'hidden'"></div>
+        </div>
+        <div class="form">
+            
+        </div>
+    </div>
+</template>
+
+<style>
+.container {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    flex-direction: column;
+    gap: 5vh;
+    height: 100% !important;
+    width: 100% !important;
+    background-image: url('./../assets/login_image.jpg');
+    max-width: none !important;
+}
+
+.welcome-msg {
+    display: flex;
+    justify-content: flex-start;
+    align-items: center;
+    flex-direction: row;
+    font-weight: 900;
+    font-family: 'Courier New, Courier, monospace';
+    font-size: 40px;
+    width: 27.5vw;
+}
+
+.visible {
+    visibility: visible;
+    border-bottom: 4px solid black; 
+}
+
+.hidden {
+    visibility: hidden;
+}
+
+.terminal-box {
+    width: 25px;
+    height: 70%;
+}
+
+.form {
+    height: 50vh;
+    width: 30vw;
+    background-color: lightgray;
+    border: 5px solid gray;
+    border-radius: 10px;
+}
+</style>
+
+<script>
+export default {
+    name: 'Login',
+    data() {
+        return {
+            username: '',
+            password: '',
+            welcomeMsgWhole: 'Welcome! Please sign in...',
+            visibleWelcomeMsg: '',
+            flicker: true
+        }
+    },
+    methods: {
+
+    },
+    mounted() {
+        this.visibleWelcomeMsg = ''
+        setInterval(() => {
+            if (this.visibleWelcomeMsg.length === this.welcomeMsgWhole.length) {
+                return;
+            }
+            let index = this.visibleWelcomeMsg.length;
+            this.visibleWelcomeMsg += this.welcomeMsgWhole[index];
+        }, 100)
+        setInterval(() => {
+            this.flicker = !this.flicker;
+        }, 500)
+        setInterval(() => {
+            this.visibleWelcomeMsg = '';
+        }, 10000)
+    }
+}
+</script>
--- src/main.js
+++ src/main.js
@@ -1,8 +1,13 @@
-import './assets/main.css'
-
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
+
+// Master CSS file
+import './components/main.css'
+
+// Bootstrap 5
+import 'bootstrap/dist/css/bootstrap.min.css'
+import 'bootstrap/dist/js/bootstrap.bundle.min.js'
 
 const app = createApp(App)
 
