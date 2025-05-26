b5ff670b17df18b2588b2496c95e06793a53e434
nms6950 <nms6950@psu.edu>
2025-05-25 22:23:30 -0400

    trying new connection

--- back-end/config/db.js
+++ back-end/config/db.js
@@ -1,20 +1,12 @@
 const { Pool } = require('pg');
 const creds = require('./db_creds');
-const dns = require('dns');
 
-// Force DNS to prefer IPv4
 const pool = new Pool({
-  host: creds.host,
-  port: creds.port,
-  database: creds.database,
-  user: creds.user,
-  password: creds.password,
-  ssl: {
-    rejectUnauthorized: false, // Required by Supabase
-  },
-  lookup: (hostname, options, callback) => {
-    dns.lookup(hostname, { family: 4 }, callback);
-  },
+  host: "aws-0-us-east-2.pooler.supabase.com",
+  port: 5432,
+  database: "postgres",
+  user: "postgres.wiwxrxvoxotkwtootgkw",
+  password: "nms6950",
 });
 
 module.exports = pool;
