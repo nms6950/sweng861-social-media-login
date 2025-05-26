aec46025ed0fc4e3bf549cecc52586ae81d03f31
nms6950 <nms6950@psu.edu>
2025-05-25 22:12:38 -0400

    lookup

--- back-end/config/db.js
+++ back-end/config/db.js
@@ -8,6 +8,9 @@ const pool = new Pool({
     user: process.env.DB_USER,
     password: process.env.DB_PASS,
     ssl: { rejectUnauthorized: false },
+    lookup: (hostname, options, callback) => {
+        return lookup(hostname, { family: 4 }, callback); // Force IPv4
+      },
 });
 
 module.exports = pool;
